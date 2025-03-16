998fd8b97c78c4fe4b79c7754a44c49d85dbfeb0
VinzentErtl <vinzent.ertl@student.htldornbirn.at>
2025-03-16 14:16:11 +0100

    state finished

--- vinzent/app/cars/page.tsx
+++ vinzent/app/cars/page.tsx
@@ -1,25 +1,65 @@
 import React from 'react'
 import Card from '../components/ui/cars/Card'
 import { Car } from '@/lib/types/types'
+import Carlist from '../components/ui/cars/Carlist';
 
 type Props = {}
 
 
-const car: Car = {
-  "name":"Super Car",
-  "type":"BMW E36",
-  "color":"black",
-  "motor":{
-    "serialNumber":1234,
-    "hp":200,
-  }
-}
+const cars: Car[] = [
+  {
+    name: "Super Car",
+    type: "BMW E36",
+    color: "black",
+    motor: {
+      serialNumber: 1234,
+      hp: 200,
+    },
+  },
+  {
+    name: "Speedster",
+    type: "Porsche 911",
+    color: "red",
+    motor: {
+      serialNumber: 5678,
+      hp: 450,
+    },
+  },
+  {
+    name: "Muscle King",
+    type: "Ford Mustang",
+    color: "blue",
+    motor: {
+      serialNumber: 9101,
+      hp: 500,
+    },
+  },
+  {
+    name: "Eco Drive",
+    type: "Tesla Model 3",
+    color: "white",
+    motor: {
+      serialNumber: 1213,
+      hp: 350,
+    },
+  },
+  {
+    name: "Offroad Beast",
+    type: "Jeep Wrangler",
+    color: "green",
+    motor: {
+      serialNumber: 1415,
+      hp: 285,
+    },
+  },
+];
+
 
 export default function page({}: Props) {
   return (
     <div>
       <h1>Cars</h1>
-      <Card car={car}/>
+      <Carlist cars={cars} />
     </div>
   )
 }
--- vinzent/app/components/ui/cars/Card.tsx
+++ vinzent/app/components/ui/cars/Card.tsx
@@ -1,10 +1,22 @@
+"use client";
 import { Car } from '@/lib/types/types';
-import React from 'react'
+import React, { useState } from 'react'
 
 type Props = {car: Car };
 
 export default function Card({car}: Props) {
+    const [isActive, setIsActive] = useState(false);
+
   return (
-    <div>Card: {car.name}</div>
+    <div 
+    className={`shadow p-4 ${isActive?"bg-amber-800":"bg-blue-500"}`} 
+    onClick={()=>{
+        setIsActive(!isActive);
+        
+    }}>
+        <h2 className='font-bold text-2xl'>{car.name}</h2>
+        <div>{car.color}</div>
+        {isActive?"active":"inactive"}
+    </div>
   )
 }
